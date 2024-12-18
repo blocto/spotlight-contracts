910a3d47a1f49240241f3c0a5ae215229476602f
shane.chi <yuhsuan19@gmail.com>
2024-12-18 15:48:32 +0800

    feat: storage contract

--- src/spotlight-token/SpotlightToken.sol
+++ src/spotlight-token/SpotlightToken.sol
@@ -2,36 +2,42 @@
 pragma solidity ^0.8.13;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {InitializableERC20} from "./InitializableERC20.sol";
 import {ISpotlightToken} from "./ISpotlightToken.sol";
 import {ISpotlightBondingCurve} from "../spotlight-bonding-curve/ISpotlightBondingCurve.sol";
 
-contract SpotlightToken is Ownable, ERC20, ISpotlightToken {
+abstract contract SpotlightTokenStorage {
+    // @dev v1 properties
     uint256 public constant BONDIGN_CURVE_SUPPLY = 800_000_000e18;
     uint256 public constant PROTOCOL_TRADING_FEE_PCT = 1; // 1%
     uint256 public constant MIN_ORDER_SIZE = 100; // 0.0001 USDC
 
     address internal _tokenCreator;
     address internal _protocolFeeRecipient;
-    address internal _bondingCurve = 0x2D6f361616a6eF15305d0099434D854f98E5cFE9;
-    address internal _baseToken = 0x40fCa9cB1AB15eD9B5bDA19A52ac00A78AE08e1D; // SUSDC
+    address internal _bondingCurve;
+    address internal _baseToken;
+    bool internal _isInitialized;
+}
 
-    constructor(address owner_, address creator_, string memory tokenName_, string memory tokenSymbol_)
-        ERC20(tokenName_, tokenSymbol_)
-        Ownable(owner_)
-    {
-        _tokenCreator = creator_;
+contract SpotlightToken is Ownable, InitializableERC20, ISpotlightToken, SpotlightTokenStorage {
+    constructor() InitializableERC20() Ownable(msg.sender) {}
+
+    function initialize(
+        address tokenCreator_,
+        address bondingCurve_,
+        address baseToken_,
+        address protocolFeeRecipient_,
+        string memory tokenName_,
+        string memory tokenSymbol_
+    ) external {
+        _tokenCreator = tokenCreator_;
+        _protocolFeeRecipient = protocolFeeRecipient_;
+        _bondingCurve = bondingCurve_;
+        _baseToken = baseToken_;
+        _tokenName = tokenName_;
+        _tokenSymbol = tokenSymbol_;
     }
-
-    // function initialize(
-    //     address protocolFeeRecipient_,
-    //     address bondingCurve_
-    // ) external onlyOwner {
-    //     _protocolFeeRecipient = protocolFeeRecipient_;
-    //     _bondingCurve = bondingCurve_;
-    //     _mint(address(this), BONDIGN_CURVE_SUPPLY);
-    // }
 
     function tokenCreator() public view returns (address) {
         return _tokenCreator;
