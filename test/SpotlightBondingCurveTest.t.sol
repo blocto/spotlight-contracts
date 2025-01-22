c033714c04f39ffdd558e10a2e0c379cf5da4b3a
Scott <scott.ph.chou@gmail.com>
2025-01-22 14:16:53 +0800

    test: fix failed tests

--- test/SpotlightBondingCurveTest.t.sol
+++ test/SpotlightBondingCurveTest.t.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.13;
 
 import "../lib/forge-std/src/Test.sol";
-import {SpotlightUSDCBondingCurve} from "../src/spotlight-bonding-curve/SpotlightUSDCBondingCurve.sol";
+import {SpotlightNativeBondingCurve} from "../src/spotlight-bonding-curve/SpotlightNativeBondingCurve.sol";
 import {ISpotlightBondingCurve} from "../src/spotlight-bonding-curve/ISpotlightBondingCurve.sol";
 
 contract SpotlightBondingCurveTest is Test {
     ISpotlightBondingCurve _bondingCurve;
 
     function setUp() public {
-        _bondingCurve = new SpotlightUSDCBondingCurve(1060848709, 4379701787);
+        _bondingCurve = new SpotlightNativeBondingCurve(1060848709, 4379701787);
     }
 
     function testGetBaseTokenBuyQuote_0() public view {
         uint256 currentSupply = 0;
-        uint256 baseTokensIn = 100_000; // 0.1 USDC
+        uint256 baseTokensIn = 0.1 ether; // 0.1 ETH
 
         uint256 targetTokensOut = _bondingCurve.getBaseTokenBuyQuote(currentSupply, baseTokensIn);
 
@@ -23,7 +23,7 @@ contract SpotlightBondingCurveTest is Test {
 
     function testGetBaseTokenBuyQuote_1() public view {
         uint256 currentSupply = 93243534057221441062232;
-        uint256 baseTokensIn = 500_000_000; // 500 USDC
+        uint256 baseTokensIn = 500 ether; // 500 ETH
 
         uint256 targetTokensOut = _bondingCurve.getBaseTokenBuyQuote(currentSupply, baseTokensIn);
 
@@ -35,7 +35,7 @@ contract SpotlightBondingCurveTest is Test {
         uint256 targetTokensOut = 100_000_000e18; // 0.1B
 
         uint256 baseTokensIn = _bondingCurve.getTargetTokenBuyQuote(currentSupply, targetTokensOut);
-        assertEq(baseTokensIn, 133113);
+        assertEq(baseTokensIn, 133113774054389860);
     }
 
     function testGetTargetTokenBuyQuote_1() public view {
@@ -43,7 +43,7 @@ contract SpotlightBondingCurveTest is Test {
         uint256 targetTokensOut = 3_000e18;
 
         uint256 baseTokensIn = _bondingCurve.getTargetTokenBuyQuote(currentSupply, targetTokensOut);
-        assertEq(baseTokensIn, 28);
+        assertEq(baseTokensIn, 28432674896780);
     }
 
     function testGetTargetTokenSellQuote_0() public view {
@@ -51,14 +51,14 @@ contract SpotlightBondingCurveTest is Test {
         uint256 targetTokensIn = 80_000e18; // 80k
 
         uint256 baseTokensOut = _bondingCurve.getTargetTokenSellQuote(currentSupply, targetTokensIn);
-        assertEq(baseTokensOut, 758);
+        assertEq(baseTokensOut, 758066870831184);
     }
 
     function testGetTargetTokenSellQuote_1() public {
         uint256 currentSupply = 10_000; // 0.5B
         uint256 targetTokensIn = 20_000; // 80k
 
-        vm.expectRevert("SpotlightUSDCBondingCureve: INSUFFICIENT_SUPPLY");
+        vm.expectRevert("SpotlightNativeBondingCurve: INSUFFICIENT_SUPPLY");
         _bondingCurve.getTargetTokenSellQuote(currentSupply, targetTokensIn);
     }
 
@@ -67,6 +67,6 @@ contract SpotlightBondingCurveTest is Test {
         uint256 targetTokensIn = 95_000e18; // 95k
 
         uint256 baseTokensOut = _bondingCurve.getTargetTokenSellQuote(currentSupply, targetTokensIn);
-        assertEq(baseTokensOut, 3349);
+        assertEq(baseTokensOut, 3349276224895252);
     }
 }
